1a6b4a53f571554337bc16c4757116d01281576a
WenZheng Li <me@wenzheng.li>
2022-03-10 00:32:27 +0900

    initial commit

---
+++ dynamic_plan_tree/src/lib.rs
@@ -0,0 +1,407 @@
+use log::debug;
+use rayon::prelude::*;
+use std::{
+    any::Any,
+    collections::HashMap,
+    time::{Duration, Instant},
+};
+
+pub trait StateMachine: Send {
+    // required
+    fn as_any(&self) -> &dyn Any;
+    fn on_run(&mut self, _plan: &mut Plan);
+    // optional
+    fn on_entry(&mut self, _plan: &mut Plan) {}
+    fn on_exit(&mut self, _plan: &mut Plan) {}
+    fn utility(&mut self, plan: &mut Plan, filter_active: bool) -> f64 {
+        plan.plans
+            .iter_mut()
+            .filter(|plan| !filter_active || plan.active)
+            .par_bridge()
+            .map(|plan| {
+                plan.call("utility", |state_machine, plan| {
+                    state_machine.utility(plan, filter_active)
+                })
+            })
+            .collect::<Vec<_>>()
+            .into_iter()
+            .fold(f64::NAN, f64::max)
+    }
+}
+
+pub fn cast<T: StateMachine + 'static>(sm: &dyn StateMachine) -> Option<&T> {
+    sm.as_any().downcast_ref::<T>()
+}
+
+pub struct Plan {
+    name: String,
+    active: bool,
+    run_timestamp: Instant,
+
+    pub run_interval: Duration,
+    pub state_machine: Box<dyn StateMachine>,
+    pub status: Option<String>,
+    pub plans: Vec<Plan>,
+    pub storage: HashMap<String, Box<dyn Any + Send>>,
+}
+
+pub struct DefaultStateMachine;
+impl StateMachine for DefaultStateMachine {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+    fn on_run(&mut self, plan: &mut Plan) {
+        // get highest utility plan
+        let best = match plan.highest_utility() {
+            Some((plan, _)) => plan.name().clone(),
+            None => return,
+        };
+        // get current plan
+        if let Some(cur) = &plan.status {
+            // current plan is already best
+            if *cur == best {
+                return;
+            }
+            // exit previous plan
+            let cur = cur.clone();
+            plan.exit(&cur);
+        }
+        // enter new plan
+        plan.enter(&best);
+        plan.status = Some(best);
+    }
+}
+
+impl Plan {
+    pub fn name(&self) -> &String {
+        &self.name
+    }
+
+    pub fn active(&self) -> bool {
+        self.active
+    }
+
+    pub fn run_timestamp(&self) -> Instant {
+        self.run_timestamp
+    }
+
+    pub fn new<S: Into<String>>(
+        state_machine: Box<dyn StateMachine>,
+        name: S,
+        active: bool,
+        run_interval: Duration,
+    ) -> Self {
+        let mut plan = Plan {
+            name: name.into(),
+            active,
+            run_timestamp: Instant::now(),
+            run_interval,
+            state_machine,
+            status: None,
+            plans: Vec::new(),
+            storage: HashMap::new(),
+        };
+        if active {
+            plan.call("on_entry", |state_machine, plan| {
+                state_machine.on_entry(plan)
+            });
+        }
+        plan
+    }
+
+    pub fn insert(&mut self, plan: Plan) -> &mut Plan {
+        // sorted insert
+        let found = self.find(&plan.name);
+        let pos = found.unwrap_or_else(|x| x);
+        debug!(
+            "{:?}\t{}\t>> insert\t{} ->\t{:?}",
+            self as *const _,
+            self.name,
+            plan.name,
+            self.plans.iter().map(|plan| &plan.name).collect::<Vec<_>>()
+        );
+        match found {
+            // overwrite if there is already one
+            Ok(_) => self.plans[pos] = plan,
+            Err(_) => self.plans.insert(pos, plan),
+        }
+        &mut self.plans[pos]
+    }
+
+    pub fn remove(&mut self, name: &str) -> Option<Plan> {
+        let pos = self.find(name).ok()?;
+        debug!(
+            "{:?}\t{}\t>> remove\t{} <-\t{:?}",
+            self as *const _,
+            self.name,
+            name,
+            self.plans.iter().map(|plan| &plan.name).collect::<Vec<_>>()
+        );
+        Some(self.plans.remove(pos))
+    }
+
+    pub fn find(&self, name: &str) -> Result<usize, usize> {
+        self.plans.binary_search_by(|plan| (*plan.name).cmp(name))
+    }
+
+    pub fn get(&self, name: &str) -> Option<&Plan> {
+        let pos = self.find(name).ok()?;
+        Some(&self.plans[pos])
+    }
+
+    pub fn run(&mut self) {
+        // call run() recursively and store status
+        self.plans
+            .iter_mut()
+            .filter(|plan| plan.active)
+            .par_bridge()
+            .for_each(|plan| plan.run());
+
+        if self.run_timestamp.elapsed() > self.run_interval {
+            // run the plan machine of this plan
+            self.run_timestamp = Instant::now();
+            self.call("on_run", |state_machine, plan| state_machine.on_run(plan));
+        }
+    }
+
+    pub fn enter(&mut self, name: &str) -> Option<&mut Plan> {
+        // can only enter plans within an active plan
+        if !self.active {
+            return None;
+        }
+        // look for requested plan
+        match self.find(name) {
+            Ok(pos) => {
+                let plan = &mut self.plans[pos];
+                // if plan is inactive, set as active and call on_entry()
+                if !plan.active {
+                    plan.active = true;
+                    plan.call("on_entry", |state_machine, plan| {
+                        state_machine.on_entry(plan)
+                    });
+                }
+                Some(plan)
+            }
+            // if plan doesn't exist, create and insert an default plan
+            Err(pos) => {
+                let default = Plan::new(
+                    Box::new(DefaultStateMachine),
+                    name,
+                    true,
+                    Duration::new(0, 0),
+                );
+                self.plans.insert(pos, default);
+                Some(&mut self.plans[pos])
+            }
+        }
+    }
+
+    pub fn exit(&mut self, name: &str) -> Option<&mut Plan> {
+        // ignore if plan is not found
+        let pos = self.find(name).ok()?;
+        let plan = &mut self.plans[pos];
+        // only exit if plan is active
+        if plan.active {
+            plan.exit_all();
+        }
+        Some(plan)
+    }
+
+    pub fn exit_all(&mut self) {
+        // recursively exit all active child plans
+        self.plans
+            .iter_mut()
+            .filter(|plan| plan.active)
+            .par_bridge()
+            .for_each(|plan| plan.exit_all());
+
+        // trigger on_exit() for self
+        self.active = false;
+        self.call("on_exit", |state_machine, plan| state_machine.on_exit(plan));
+    }
+
+    pub fn highest_utility(&mut self) -> Option<(&Plan, f64)> {
+        if self.plans.is_empty() {
+            None
+        } else {
+            let (pos, utility) = self
+                .plans
+                .par_iter_mut()
+                .map(|plan| {
+                    plan.call("utility", |state_machine, plan| {
+                        state_machine.utility(plan, false)
+                    })
+                })
+                .enumerate()
+                .collect::<Vec<_>>()
+                .into_iter()
+                .fold((0, f64::NAN), |max, x| if max.1 > x.1 { max } else { x });
+            Some((&self.plans[pos], utility))
+        }
+    }
+
+    pub fn debug_log(&self, pre: &str, tag: &str) {
+        debug!(
+            "{:?}\t{}\t{} {}\t{:?}\t{:?}",
+            self as *const _,
+            self.name,
+            pre,
+            tag,
+            self.status,
+            self.plans
+                .iter()
+                .filter(|plan| plan.active)
+                .map(|plan| &plan.name)
+                .collect::<Vec<_>>()
+        );
+    }
+
+    fn call<F, T>(&mut self, f_name: &str, f: F) -> T
+    where
+        F: FnOnce(&mut Box<dyn StateMachine>, &mut Plan) -> T,
+    {
+        self.debug_log(">>", f_name);
+        let mut state_machine =
+            std::mem::replace(&mut self.state_machine, Box::new(DefaultStateMachine));
+        let ret = f(&mut state_machine, self);
+        let _ = std::mem::replace(&mut self.state_machine, state_machine);
+        self.debug_log("<<", f_name);
+        ret
+    }
+}
+
+impl Drop for Plan {
+    fn drop(&mut self) {
+        if self.active {
+            self.active = false;
+            self.call("on_exit", |state_machine, plan| state_machine.on_exit(plan));
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[derive(Default)]
+    struct TestStateMachine {
+        entry_count: u32,
+        exit_count: u32,
+        run_count: u32,
+    }
+
+    impl StateMachine for TestStateMachine {
+        fn as_any(&self) -> &dyn Any {
+            self
+        }
+        fn on_entry(&mut self, _plan: &mut Plan) {
+            self.entry_count += 1;
+        }
+        fn on_exit(&mut self, _plan: &mut Plan) {
+            self.exit_count += 1;
+        }
+        fn on_run(&mut self, plan: &mut Plan) {
+            self.run_count += 1;
+            let active_plans = plan
+                .plans
+                .iter()
+                .filter(|plan| plan.active())
+                .map(|plan| plan.name().clone())
+                .collect::<Vec<_>>();
+
+            for x in active_plans {
+                match x.as_str() {
+                    "A" => {
+                        plan.exit("A");
+                        plan.enter("B");
+                    }
+                    "B" => {
+                        plan.exit("B");
+                        plan.enter("C");
+                    }
+                    "C" => {
+                        plan.exit("C");
+                        plan.enter("A");
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    fn new_plan(name: &str, active: bool) -> Plan {
+        Plan::new(
+            Box::new(TestStateMachine::default()),
+            name,
+            active,
+            Duration::new(0, 0),
+        )
+    }
+
+    #[test]
+    fn sorted_insert() {
+        let _ = env_logger::try_init();
+
+        let mut root_plan = new_plan("root", true);
+        root_plan.insert(new_plan("C", true));
+        root_plan.insert(new_plan("A", true));
+        root_plan.insert(new_plan("B", true));
+        root_plan.insert(new_plan("B", true));
+
+        assert_eq!(root_plan.plans.len(), 3);
+        for (i, plan) in root_plan.plans.iter().enumerate() {
+            assert!(plan.active());
+            assert_eq!(plan.name(), &((b'A' + (i as u8)) as char).to_string());
+            let sm = cast::<TestStateMachine>(&*plan.state_machine).unwrap();
+            assert_eq!(sm.entry_count, 1);
+            assert_eq!(sm.run_count, 0);
+            assert_eq!(sm.exit_count, 0);
+        }
+        root_plan.exit_all();
+        for plan in &root_plan.plans {
+            assert!(!plan.active());
+            let sm = cast::<TestStateMachine>(&*plan.state_machine).unwrap();
+            assert_eq!(sm.exit_count, 1);
+        }
+    }
+
+    #[test]
+    fn cycle_plans() {
+        let _ = env_logger::try_init();
+        let mut root_plan = new_plan("root", true);
+        // init plan to A
+        root_plan.insert(new_plan("A", true));
+        root_plan.insert(new_plan("B", false));
+        root_plan.insert(new_plan("C", false));
+        root_plan.run();
+        root_plan.run();
+        let cycles = 10;
+        for _ in 0..(cycles - 1) {
+            assert!(!root_plan.get("A").unwrap().active());
+            assert!(!root_plan.get("B").unwrap().active());
+            assert!(root_plan.get("C").unwrap().active());
+            root_plan.run();
+            assert!(root_plan.get("A").unwrap().active());
+            assert!(!root_plan.get("B").unwrap().active());
+            assert!(!root_plan.get("C").unwrap().active());
+            root_plan.run();
+            assert!(!root_plan.get("A").unwrap().active());
+            assert!(root_plan.get("B").unwrap().active());
+            assert!(!root_plan.get("C").unwrap().active());
+            root_plan.run();
+        }
+        root_plan.exit_all();
+        for plan in &root_plan.plans {
+            let sm = cast::<TestStateMachine>(&*plan.state_machine).unwrap();
+            assert_eq!(sm.entry_count, cycles);
+            assert_eq!(sm.exit_count, cycles);
+            // off by one becase inital plan didn't run
+            let run_cycles = if plan.name() == "C" {
+                cycles - 1
+            } else {
+                cycles
+            };
+            assert_eq!(sm.run_count, run_cycles);
+        }
+    }
+}
